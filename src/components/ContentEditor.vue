58467921081ab4dca8375b1b481fd9f814e54cdd
muyids <289869876@qq.com>
2025-10-29 21:57:17 +0800

    free

--- src/components/ContentEditor.vue
+++ src/components/ContentEditor.vue
@@ -30,6 +30,8 @@ const config = computed(() => ({
       '|',
       'bulletedList', 'numberedList', 'blockQuote',
       '|',
+      'alignment',
+      '|',
       'undo', 'redo'
     ],
     shouldNotGroupWhenFull: true
