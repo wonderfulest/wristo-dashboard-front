f9269ae90d79d841f36e440cc0fa1d04cd30aaaf
muyids <289869876@qq.com>
2026-02-08 11:23:34 +0800

    add google auth

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,5 +2,5 @@ import instance from '@/config/axios'
 import type { ApiResponse } from '@/types/api'
 
 export const logout = async () : Promise<ApiResponse<string>> => {
-  return instance.post('/public/auth/logout')
+  return instance.post('/auth/logout')
 }
